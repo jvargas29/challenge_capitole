ac83ef09eeef33d80a80bc946bf923fbbd4a1a6e
jvargas29 <yuliana.vargas29@gmail.com>
2023-02-24 23:06:37 -0300

    correction openapi.yml in content type schema Error. Modify pom.xml: Remove springfox and swagger 2 dependencies is not necessary to swagger 3. Source: https://springdoc.org/v2/#migrating-from-springfox Add openapi-generator-ignore file Add PricesApiController empty class

---
+++ src/main/java/com/capitole/challenge/rest/PricesApiController.java
@@ -0,0 +1,9 @@
+package com.capitole.challenge.rest;
+
+import com.capitole.challenge.api.PricesApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PricesApiController implements PricesApi {
+
+}
