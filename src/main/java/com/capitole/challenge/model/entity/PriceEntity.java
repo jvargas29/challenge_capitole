da62f73d72c863453fd91a281328f09ad9723056
jvargas29 <yuliana.vargas29@gmail.com>
2023-02-26 18:11:41 -0300

    Add entity price. Add configuration default h2.

---
+++ src/main/java/com/capitole/challenge/model/entity/PriceEntity.java
@@ -0,0 +1,45 @@
+package com.capitole.challenge.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name="PRICE")
+public class PriceEntity {
+    @Id
+    @Column(name = "PRICE_ID")
+    private Long id;
+
+    @Column(name = "BRAND_ID")
+    private Long brandId;
+
+    @Column(name = "START_DATE")
+    private LocalDateTime startDate;
+
+    @Column(name = "END_DATE")
+    private LocalDateTime endDate;
+
+    @Column(name = "PRICE_LIST_ID")
+    private Long priceListId;
+
+    @Column(name = "PRODUCT_ID")
+    private Long productId;
+
+    @Column(name = "PRIORITY")
+    private Integer priority;
+
+    @Column(name = "PRICE")
+    private Double price;
+
+    @Column(name = "CURRENCY")
+    private String currency;
+
+}
